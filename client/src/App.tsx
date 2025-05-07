451aa863aa227f2c660c7fdc22bb00e01bc06f33
zalven-official <zalvendayao888@gmail.com>
2025-05-08 00:18:45 +0800

    finishign req

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,19 @@
 "use client"
 
-import { useEffect, useRef } from "react";
+import SampleForm from "./components/sample/form"
+import SampleMedia from "./components/sample/media"
+
+
 
 function App() {
-  const socketRef = useRef<WebSocket | null>(null);
 
-  useEffect(() => {
-
-    const socket = new WebSocket(import.meta.env.VITE_BROADCAST_URL);
-    socketRef.current = socket;
-
-    socket.onopen = () => {
-      console.log('WebSocket connected');
-
-      // Example: subscribe to a topic
-      socket.send(JSON.stringify({ topic: 'chat.general' }));
-    };
-
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log('Received:', data);
-    };
-
-    socket.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
-
-    socket.onclose = () => {
-      console.log('WebSocket closed');
-    };
-
-    return () => {
-      socket.close();
-    };
-  }, []);
     return (
       <div>
         <div>
-          {/* <SampleMedia/> */}
+          <SampleMedia/>
         </div>
         <div>
-          {/* <SampleForm/> */}
+          <SampleForm/>
         </div>
       </div>
     )
