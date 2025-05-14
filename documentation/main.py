fd46a5f007d4735eceff240c11c970e21a43a3c7
zalven-official <zalvendayao888@gmail.com>
2025-05-15 03:36:34 +0800

    finished member profile

---
+++ documentation/main.py
@@ -0,0 +1,30 @@
+
+def main():
+    txt = """
+memberGroupG := service.Group("/member-group")
+	{
+		memberGroupG.GET("", c.MemberGroupList)
+		memberGroupG.GET("/:member_group_id", c.MemberGroupGetByID)
+		memberGroupG.POST("", c.MemberGroupCreate)
+		memberGroupG.PUT("/:member_group_id", c.MemberGroupUpdate)
+		memberGroupG.DELETE("/:member_group_id", c.MemberGroupDelete)
+		memberGroupG.GET("/branch/:branch_id", c.MemberGroupListByBranch)
+		memberGroupG.GET("/organization/:organization_id", c.MemberGroupListByOrganization)
+		memberGroupG.GET("/organization/:organization_id/branch/:branch_id", c.MemberGroupListByOrganizationBranch)
+	}
+
+"""
+    replacements = [
+        ("member_group", "member_profile"),
+        ("member-group", "member-profile"),
+        ("memberGroup", "memberProfile"),
+        ("MemberGroup", "MemberProfile"),
+    ]
+    for (from_change, to_change) in replacements:
+        txt = txt.replace(from_change, to_change)
+
+    print(txt)
+
+
+if __name__ == "__main__":
+    main()
