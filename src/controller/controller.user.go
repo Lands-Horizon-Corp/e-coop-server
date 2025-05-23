141331718d94c6d1ce6d27f58ab180ba55cbbb27
zalven-official <zalvendayao888@gmail.com>
2025-05-24 06:30:04 +0800

    adding auth

--- src/controller/controller.user.go
+++ src/controller/controller.user.go
@@ -104,7 +104,7 @@ func (c *Controller) UserController() {
 	req.RegisterRoute(horizon.Route{
 		Route:  "/authentication/verify-with-email",
 		Method: "POST",
-		Note:   "Verify with Email: this is used to verify the user's email by sending OTP to email.",
+		Note:   "Verify with Email: this is used to verify the user's email by sending OTP to email.  [for preceeding protected self actions]",
 	}, func(c echo.Context) error {
 		return nil
 	})
@@ -113,7 +113,7 @@ func (c *Controller) UserController() {
 		Route:   "/authentication/verify-with-email-confirmation",
 		Method:  "POST",
 		Request: "6 digit OTP",
-		Note:    "Verify with Email Confirmation: this is used to confirm the OTP sent to the user's email.",
+		Note:    "Verify with Email Confirmation: this is used to confirm the OTP sent to the user's email.  [for preceeding protected self actions]",
 	}, func(c echo.Context) error {
 		return nil
 	})
@@ -121,7 +121,7 @@ func (c *Controller) UserController() {
 	req.RegisterRoute(horizon.Route{
 		Route:  "/authentication/verify-with-contact",
 		Method: "POST",
-		Note:   "Verify with Contact: this is used to verify the user's contact number by sending OTP.",
+		Note:   "Verify with Contact: this is used to verify the user's contact number by sending OTP.  [for preceeding protected self actions]",
 	}, func(c echo.Context) error {
 		return nil
 	})
@@ -130,7 +130,7 @@ func (c *Controller) UserController() {
 		Route:   "/authentication/verify-with-contact-confirmation",
 		Method:  "POST",
 		Request: "6 digit OTP",
-		Note:    "Verify with Contact Confirmation: this is used to confirm the OTP sent to the user's contact number.",
+		Note:    "Verify with Contact Confirmation: this is used to confirm the OTP sent to the user's contact number.  [for preceeding protected self actions]",
 	}, func(c echo.Context) error {
 		return nil
 	})
@@ -139,9 +139,63 @@ func (c *Controller) UserController() {
 		Route:   "/authentication/verify-with-password",
 		Method:  "POST",
 		Request: "password & password confirmation",
-		Note:    "Verify with Password: this is used to verify the user's password.",
+		Note:    "Verify with Password: this is used to verify the user's password. [for preceeding protected self actions]",
 	}, func(c echo.Context) error {
 		return nil
 	})
 
+	req.RegisterRoute(horizon.Route{
+		Route:   "/profile/password",
+		Method:  "PUT",
+		Request: "IChangePasswordRequest",
+		Note:    "Change Password: this is used to change the user's password.",
+	}, func(c echo.Context) error {
+		return nil
+	})
+
+	req.RegisterRoute(horizon.Route{
+		Route:    "/profile/email",
+		Method:   "PUT",
+		Request:  "IChangeEmailRequest",
+		Response: "TUser",
+		Note:     "Change Email: this is used to change the user's email.",
+	}, func(c echo.Context) error {
+		return nil
+	})
+	req.RegisterRoute(horizon.Route{
+		Route:    "/profile/username",
+		Method:   "PUT",
+		Request:  "IChangeUsernameRequest",
+		Response: "TUser",
+		Note:     "Change Username: this is used to change the user's username.",
+	}, func(c echo.Context) error {
+		return nil
+	})
+	req.RegisterRoute(horizon.Route{
+		Route:    "/profile/contact-number",
+		Method:   "PUT",
+		Request:  "IChangeContactNumberRequest",
+		Response: "TUser",
+		Note:     "Change Contact Number: this is used to change the user's contact number.",
+	}, func(c echo.Context) error {
+		return nil
+	})
+	req.RegisterRoute(horizon.Route{
+		Route:    "/profile/profile-picture",
+		Method:   "PUT",
+		Request:  "IUserSettingsPhotoUpdateRequest",
+		Response: "TUser",
+		Note:     "Change Profile Picture: this is used to change the user's profile picture.",
+	}, func(c echo.Context) error {
+		return nil
+	})
+	req.RegisterRoute(horizon.Route{
+		Route:    "/profile/general",
+		Method:   "PUT",
+		Request:  "IUserSettingsGeneralRequest",
+		Response: "TUser",
+		Note:     "Change General Settings: this is used to change the user's general settings.",
+	}, func(c echo.Context) error {
+		return nil
+	})
 }
