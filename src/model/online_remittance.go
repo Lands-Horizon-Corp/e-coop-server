4efbc250dda678de37bdc6ce9a23fd98f47f3d45
z-dayao <zalvendayao888@gmail.com>
2025-07-01 18:59:21 +0800

    fix val

--- src/model/online_remittance.go
+++ src/model/online_remittance.go
@@ -74,8 +74,6 @@ type (
 	}
 
 	OnlineRemittanceRequest struct {
-		OrganizationID     uuid.UUID  `json:"organization_id" validate:"required"`
-		BranchID           uuid.UUID  `json:"branch_id" validate:"required"`
 		BankID             uuid.UUID  `json:"bank_id" validate:"required"`
 		MediaID            *uuid.UUID `json:"media_id,omitempty"`
 		EmployeeUserID     *uuid.UUID `json:"employee_user_id,omitempty"`
