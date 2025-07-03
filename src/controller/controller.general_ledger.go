4647bcbc0ab7efd71e2c4f813b7e8c28c17f9f8b
z-dayao <zalvendayao888@gmail.com>
2025-07-04 00:48:23 +0800

    indexing

--- src/controller/controller.general_ledger.go
+++ src/controller/controller.general_ledger.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -11,30 +12,6 @@ import (
 
 func (c *Controller) GeneralLedgerController() {
 	req := c.provider.Service.Request
-
-	req.RegisterRoute(horizon.Route{
-		Route:    "/general-ledger-definition",
-		Method:   "GET",
-		Response: "GeneralLedgerDefinition[]",
-		Note:     "List all general ledger definitions for the current branch",
-	}, func(ctx echo.Context) error {
-		context := ctx.Request().Context()
-		userOrg, err := c.userOrganizationToken.CurrentUserOrganization(context, ctx)
-		if err != nil {
-			return err
-		}
-		if userOrg.UserType != "owner" && userOrg.UserType != "employee" {
-			return c.BadRequest(ctx, "User is not authorized")
-		}
-		gl, err := c.model.GeneralLedgerDefinitionManager.FindRaw(context, &model.GeneralLedgerDefinition{
-			OrganizationID: userOrg.OrganizationID,
-			BranchID:       *userOrg.BranchID,
-		})
-		if err != nil {
-			return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-		}
-		return ctx.JSON(http.StatusOK, gl)
-	})
 
 	req.RegisterRoute(horizon.Route{
 		Route:    "/general-ledger-accounts-grouping",
@@ -51,6 +28,30 @@ func (c *Controller) GeneralLedgerController() {
 			return c.BadRequest(ctx, "User is not authorized")
 		}
 		gl, err := c.model.GeneralLedgerAccountsGroupingManager.FindRaw(context, &model.GeneralLedgerAccountsGrouping{
+			OrganizationID: userOrg.OrganizationID,
+			BranchID:       *userOrg.BranchID,
+		})
+		if err != nil {
+			return ctx.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+		return ctx.JSON(http.StatusOK, gl)
+	})
+
+	req.RegisterRoute(horizon.Route{
+		Route:    "/general-ledger-definition",
+		Method:   "GET",
+		Response: "GeneralLedgerDefinition[]",
+		Note:     "List all general ledger definitions for the current branch",
+	}, func(ctx echo.Context) error {
+		context := ctx.Request().Context()
+		userOrg, err := c.userOrganizationToken.CurrentUserOrganization(context, ctx)
+		if err != nil {
+			return err
+		}
+		if userOrg.UserType != "owner" && userOrg.UserType != "employee" {
+			return c.BadRequest(ctx, "User is not authorized")
+		}
+		gl, err := c.model.GeneralLedgerDefinitionManager.FindRaw(context, &model.GeneralLedgerDefinition{
 			OrganizationID: userOrg.OrganizationID,
 			BranchID:       *userOrg.BranchID,
 		})
@@ -108,6 +109,116 @@ func (c *Controller) GeneralLedgerController() {
 
 		// Create the general ledger definition
 		if err := c.model.GeneralLedgerDefinitionManager.Create(context, glDefinition); err != nil {
+			return c.InternalServerError(ctx, err)
+		}
+
+		return ctx.JSON(http.StatusOK, c.model.GeneralLedgerDefinitionManager.ToModel(glDefinition))
+	})
+
+	req.RegisterRoute(horizon.Route{
+		Route:    "/general-ledger-definition/:general_ledger_definition_id",
+		Method:   "PUT",
+		Request:  "GeneralLedgerDefinitionRequest",
+		Response: "GeneralLedgerDefinitionResponse",
+		Note:     "Update an existing general ledger definition",
+	}, func(ctx echo.Context) error {
+		context := ctx.Request().Context()
+
+		// Get and validate general ledger definition ID
+		glDefinitionID, err := horizon.EngineUUIDParam(ctx, "general_ledger_definition_id")
+		if err != nil {
+			return c.BadRequest(ctx, "Invalid general ledger definition ID")
+		}
+
+		// Validate request
+		req, err := c.model.GeneralLedgerDefinitionManager.Validate(ctx)
+		if err != nil {
+			return c.BadRequest(ctx, err.Error())
+		}
+
+		// Get current user organization
+		userOrg, err := c.userOrganizationToken.CurrentUserOrganization(context, ctx)
+		if err != nil {
+			return err
+		}
+
+		// Check authorization
+		if userOrg.UserType != "owner" && userOrg.UserType != "employee" {
+			return c.BadRequest(ctx, "User is not authorized")
+		}
+
+		// Get existing general ledger definition
+		glDefinition, err := c.model.GeneralLedgerDefinitionManager.GetByID(context, *glDefinitionID)
+		if err != nil {
+			return c.NotFound(ctx, "General Ledger Definition")
+		}
+
+		// Update fields
+		glDefinition.ParentID = req.GeneralLedgerDefinitionEntriesID
+		glDefinition.GeneralLedgerAccountsGroupingID = req.GeneralLedgerAccountsGroupingID
+		glDefinition.Name = req.Name
+		glDefinition.Description = req.Description
+		glDefinition.Index = req.Index
+		glDefinition.NameInTotal = req.NameInTotal
+		glDefinition.IsPosting = req.IsPosting
+		glDefinition.GeneralLedgerType = req.GeneralLedgerType
+		glDefinition.BeginningBalanceOfTheYearCredit = req.BeginningBalanceOfTheYearCredit
+		glDefinition.BeginningBalanceOfTheYearDebit = req.BeginningBalanceOfTheYearDebit
+		glDefinition.UpdatedAt = time.Now().UTC()
+		glDefinition.UpdatedByID = userOrg.UserID
+
+		// Update the general ledger definition
+		if err := c.model.GeneralLedgerDefinitionManager.UpdateFields(context, glDefinition.ID, glDefinition); err != nil {
+			return c.InternalServerError(ctx, err)
+		}
+
+		return ctx.JSON(http.StatusOK, c.model.GeneralLedgerDefinitionManager.ToModel(glDefinition))
+	})
+
+	req.RegisterRoute(horizon.Route{
+		Route:    "/general-ledger-definition/:general_ledger_definition_id/index/:index",
+		Method:   "PUT",
+		Response: "GeneralLedgerDefinitionResponse",
+		Note:     "Update the index of a general ledger definition",
+	}, func(ctx echo.Context) error {
+		context := ctx.Request().Context()
+
+		// Get and validate general ledger definition ID
+		glDefinitionID, err := horizon.EngineUUIDParam(ctx, "general_ledger_definition_id")
+		if err != nil {
+			return c.BadRequest(ctx, "Invalid general ledger definition ID")
+		}
+
+		// Get and validate index parameter
+		index, err := strconv.Atoi(ctx.Param("index"))
+		if err != nil {
+			return c.BadRequest(ctx, "Invalid index value")
+		}
+
+		// Get current user organization
+		userOrg, err := c.userOrganizationToken.CurrentUserOrganization(context, ctx)
+		if err != nil {
+			return err
+		}
+
+		// Check authorization
+		if userOrg.UserType != "owner" && userOrg.UserType != "employee" {
+			return c.BadRequest(ctx, "User is not authorized")
+		}
+
+		// Get existing general ledger definition
+		glDefinition, err := c.model.GeneralLedgerDefinitionManager.GetByID(context, *glDefinitionID)
+		if err != nil {
+			return c.NotFound(ctx, "General Ledger Definition")
+		}
+
+		// Update only the index and audit fields
+		glDefinition.Index = index
+		glDefinition.UpdatedAt = time.Now().UTC()
+		glDefinition.UpdatedByID = userOrg.UserID
+
+		// Update the general ledger definition
+		if err := c.model.GeneralLedgerDefinitionManager.UpdateFields(context, glDefinition.ID, glDefinition); err != nil {
 			return c.InternalServerError(ctx, err)
 		}
 
