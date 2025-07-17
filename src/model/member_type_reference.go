48c75951a873d69d155296431dd06c49f8d2ae9a
z-dayao <zalvendayao888@gmail.com>
2025-07-17 14:09:36 +0800

    adding reference json

--- src/model/member_type_reference.go
+++ src/model/member_type_reference.go
@@ -12,37 +12,37 @@ import (
 
 type (
 	MemberTypeReference struct {
-		ID          uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-		CreatedAt   time.Time      `gorm:"not null;default:now()"`
-		CreatedByID uuid.UUID      `gorm:"type:uuid"`
+		ID          uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+		CreatedAt   time.Time      `gorm:"not null;default:now()" json:"created_at"`
+		CreatedByID uuid.UUID      `gorm:"type:uuid" json:"created_by_id"`
 		CreatedBy   *User          `gorm:"foreignKey:CreatedByID;constraint:OnDelete:SET NULL;" json:"created_by,omitempty"`
-		UpdatedAt   time.Time      `gorm:"not null;default:now()"`
-		UpdatedByID uuid.UUID      `gorm:"type:uuid"`
+		UpdatedAt   time.Time      `gorm:"not null;default:now()" json:"updated_at"`
+		UpdatedByID uuid.UUID      `gorm:"type:uuid" json:"updated_by_id"`
 		UpdatedBy   *User          `gorm:"foreignKey:UpdatedByID;constraint:OnDelete:SET NULL;" json:"updated_by,omitempty"`
-		DeletedAt   gorm.DeletedAt `gorm:"index"`
-		DeletedByID *uuid.UUID     `gorm:"type:uuid"`
+		DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+		DeletedByID *uuid.UUID     `gorm:"type:uuid" json:"deleted_by_id,omitempty"`
 		DeletedBy   *User          `gorm:"foreignKey:DeletedByID;constraint:OnDelete:SET NULL;" json:"deleted_by,omitempty"`
 
-		OrganizationID uuid.UUID     `gorm:"type:uuid;not null;index:idx_organization_branch_member_type_reference"`
+		OrganizationID uuid.UUID     `gorm:"type:uuid;not null;index:idx_organization_branch_member_type_reference" json:"organization_id"`
 		Organization   *Organization `gorm:"foreignKey:OrganizationID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE;" json:"organization,omitempty"`
-		BranchID       uuid.UUID     `gorm:"type:uuid;not null;index:idx_organization_branch_member_type_reference"`
+		BranchID       uuid.UUID     `gorm:"type:uuid;not null;index:idx_organization_branch_member_type_reference" json:"branch_id"`
 		Branch         *Branch       `gorm:"foreignKey:BranchID;constraint:OnDelete:CASCADE,OnUpdate:CASCADE;" json:"branch,omitempty"`
 
-		AccountID uuid.UUID `gorm:"type:uuid"`
+		AccountID uuid.UUID `gorm:"type:uuid" json:"account_id"`
 		Account   *Account  `gorm:"foreignKey:AccountID;constraint:OnDelete:RESTRICT,OnUpdate:CASCADE;" json:"account,omitempty"`
 
-		MemberTypeID uuid.UUID   `gorm:"type:uuid"`
+		MemberTypeID uuid.UUID   `gorm:"type:uuid" json:"member_type_id"`
 		MemberType   *MemberType `gorm:"foreignKey:MemberTypeID;constraint:OnDelete:RESTRICT,OnUpdate:CASCADE;" json:"member_type,omitempty"`
 
-		MaintainingBalance                             float64 `gorm:"type:decimal;default:0"`
-		Description                                    string  `gorm:"type:text"`
-		InterestRate                                   float64 `gorm:"type:decimal;default:0"`
-		MinimumBalance                                 float64 `gorm:"type:decimal;default:0"`
-		Charges                                        float64 `gorm:"type:decimal;default:0"`
-		ActiveMemberMinimumBalance                     float64 `gorm:"type:decimal;default:0"`
-		ActiveMemberRatio                              float64 `gorm:"type:decimal;default:0"`
-		OtherInterestOnSavingComputationMinimumBalance float64 `gorm:"type:decimal;default:0"`
-		OtherInterestOnSavingComputationInterestRate   float64 `gorm:"type:decimal;default:0"`
+		MaintainingBalance                             float64 `gorm:"type:decimal;default:0" json:"maintaining_balance"`
+		Description                                    string  `gorm:"type:text" json:"description"`
+		InterestRate                                   float64 `gorm:"type:decimal;default:0" json:"interest_rate"`
+		MinimumBalance                                 float64 `gorm:"type:decimal;default:0" json:"minimum_balance"`
+		Charges                                        float64 `gorm:"type:decimal;default:0" json:"charges"`
+		ActiveMemberMinimumBalance                     float64 `gorm:"type:decimal;default:0" json:"active_member_minimum_balance"`
+		ActiveMemberRatio                              float64 `gorm:"type:decimal;default:0" json:"active_member_ratio"`
+		OtherInterestOnSavingComputationMinimumBalance float64 `gorm:"type:decimal;default:0" json:"other_interest_on_saving_computation_minimum_balance"`
+		OtherInterestOnSavingComputationInterestRate   float64 `gorm:"type:decimal;default:0" json:"other_interest_on_saving_computation_interest_rate"`
 	}
 
 	MemberTypeReferenceResponse struct {
