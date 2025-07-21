6394f68048c90e81998b199f6f4b11d74da82b55
z-dayao <zalvendayao888@gmail.com>
2025-07-21 10:01:41 +0800

    feat: adding request

--- services/horizon/horizon.request.go
+++ services/horizon/horizon.request.go
@@ -69,9 +69,25 @@ var (
 		".env", ".yaml", ".yml", ".ini", ".config", ".conf", ".xml", ".git",
 		".htaccess", ".htpasswd", ".backup", ".secret", ".credential", ".password",
 		".private", ".key", ".token", ".dump", ".database", ".db", ".logs", ".debug",
+		".pem", ".crt", ".cert", ".pfx", ".p12", ".bak", ".swp", ".tmp", ".cache",
+		".session", ".sqlite", ".sqlite3", ".mdf", ".ldf", ".rdb", ".ldb", ".log",
+		".old", ".orig", ".example", ".sample", ".test", ".spec", ".out", ".core",
 	}
 	forbiddenSubstrings = []string{
 		"dockerfile",
+		"secrets",
+		"credentials",
+		"password",
+		"private",
+		"config",
+		"backup",
+		"token",
+		"key",
+		"certificate",
+		"ssl",
+		"auth",
+		"hidden",
+		"admin",
 	}
 )
 
