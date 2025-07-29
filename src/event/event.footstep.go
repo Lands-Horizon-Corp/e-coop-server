cdbfae3c667e77725906a11d8ca43c9733116b8d
z-dayao <zalvendayao888@gmail.com>
2025-07-29 14:35:32 +0800

    footstep logs

--- src/event/event.footstep.go
+++ src/event/event.footstep.go
@@ -3,8 +3,10 @@ package event
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lands-horizon/horizon-server/src/model"
 )
 
 type FootstepEvent struct {
@@ -17,56 +19,56 @@ func (e *Event) Footstep(context context.Context, ctx echo.Context, data Footste
 	fmt.Println("[Footstep] Logging event:", data.Activity, data.Module, data.Description)
 
 	go func() {
-		// fmt.Println("[Footstep] Logging event:", data.Activity, data.Module, data.Description)
+		fmt.Println("[Footstep] Logging event:", data.Activity, data.Module, data.Description)
 
-		// user, err := e.userToken.CurrentUser(context, ctx)
-		// if err != nil {
-		// 	fmt.Println("Failed to get current user:", err)
-		// 	return
-		// }
+		user, err := e.userToken.CurrentUser(context, ctx)
+		if err != nil {
+			fmt.Println("Failed to get current user:", err)
+			return
+		}
 
-		// userOrganization, _ := e.userOrganizationToken.CurrentUserOrganization(context, ctx)
+		userOrganization, _ := e.userOrganizationToken.CurrentUserOrganization(context, ctx)
 
-		// userId := user.ID
+		userId := user.ID
 
-		// var userType string
-		// if userOrganization != nil {
-		// 	userType = userOrganization.UserType
-		// }
+		var userType string
+		if userOrganization != nil {
+			userType = userOrganization.UserType
+		}
 
-		// claim, _ := e.userToken.CSRF.GetCSRF(context, ctx)
-		// latitude := claim.Latitude
-		// longitude := claim.Longitude
-		// ipAddress := claim.IPAddress
-		// userAgent := claim.UserAgent
-		// referer := claim.Referer
-		// location := claim.Location
-		// acceptLanguage := claim.AcceptLanguage
+		claim, _ := e.userToken.CSRF.GetCSRF(context, ctx)
+		latitude := claim.Latitude
+		longitude := claim.Longitude
+		ipAddress := claim.IPAddress
+		userAgent := claim.UserAgent
+		referer := claim.Referer
+		location := claim.Location
+		acceptLanguage := claim.AcceptLanguage
 
-		// if err := e.model.FootstepManager.Create(context, &model.Footstep{
-		// 	CreatedAt:      time.Now().UTC(),
-		// 	CreatedByID:    userId,
-		// 	UpdatedAt:      time.Now().UTC(),
-		// 	UpdatedByID:    userId,
-		// 	OrganizationID: &userOrganization.OrganizationID,
-		// 	BranchID:       userOrganization.BranchID,
-		// 	UserID:         &userId,
-		// 	Description:    data.Description,
-		// 	Activity:       data.Activity,
-		// 	UserType:       userType,
-		// 	Module:         data.Module,
-		// 	Latitude:       &latitude,
-		// 	Longitude:      &longitude,
-		// 	Timestamp:      time.Now().UTC(),
-		// 	IPAddress:      ipAddress,
-		// 	UserAgent:      userAgent,
-		// 	Referer:        referer,
-		// 	Location:       location,
-		// 	AcceptLanguage: acceptLanguage,
-		// }); err != nil {
-		// 	fmt.Println("Failed to save footstep:", err)
-		// 	return
-		// }
-		// fmt.Println("[Footstep] Event saved successfully!") // <-- Add this line
+		if err := e.model.FootstepManager.Create(context, &model.Footstep{
+			CreatedAt:      time.Now().UTC(),
+			CreatedByID:    userId,
+			UpdatedAt:      time.Now().UTC(),
+			UpdatedByID:    userId,
+			OrganizationID: &userOrganization.OrganizationID,
+			BranchID:       userOrganization.BranchID,
+			UserID:         &userId,
+			Description:    data.Description,
+			Activity:       data.Activity,
+			UserType:       userType,
+			Module:         data.Module,
+			Latitude:       &latitude,
+			Longitude:      &longitude,
+			Timestamp:      time.Now().UTC(),
+			IPAddress:      ipAddress,
+			UserAgent:      userAgent,
+			Referer:        referer,
+			Location:       location,
+			AcceptLanguage: acceptLanguage,
+		}); err != nil {
+			fmt.Println("Failed to save footstep:", err)
+			return
+		}
+		fmt.Println("[Footstep] Event saved successfully!") // <-- Add this line
 	}()
 }
