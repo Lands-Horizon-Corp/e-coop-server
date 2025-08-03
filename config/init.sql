da152d4f9952bc48410746dd35f2b48ae3e53ba5
z-dayao <zalvendayao888@gmail.com>
2025-08-04 05:55:45 +0800

    updating packages

--- config/init.sql
+++ config/init.sql
@@ -1,2 +1,5 @@
 -- Enable the pgcrypto extension to use gen_random_uuid
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- Enable the vector extension for vector operations
+CREATE EXTENSION IF NOT EXISTS vector;
