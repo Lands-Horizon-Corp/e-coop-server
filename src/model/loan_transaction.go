b41b425e182dff8edba1092c995d4b6dfb12594d
z-dayao <zalvendayao888@gmail.com>
2025-09-19 14:17:40 +0800

    fix: collateral entity

--- src/model/loan_transaction.go
+++ src/model/loan_transaction.go
@@ -562,7 +562,7 @@ func (m *Model) LoanTransaction() {
 		Preloads: []string{
 			"CreatedBy", "UpdatedBy", "Branch", "Organization", "EmployeeUser",
 			"TransactionBatch", "LoanPurpose", "LoanStatus",
-			"ComakerDepositMemberAccountingLedger", "ComakerCollateral", "PreviousLoan",
+			"ComakerDepositMemberAccountingLedger", "PreviousLoan",
 			"Account", "MemberProfile", "MemberJointAccount", "SignatureMedia",
 			"ApprovedBySignatureMedia", "PreparedBySignatureMedia", "CertifiedBySignatureMedia",
 			"VerifiedBySignatureMedia", "CheckBySignatureMedia", "AcknowledgeBySignatureMedia",
