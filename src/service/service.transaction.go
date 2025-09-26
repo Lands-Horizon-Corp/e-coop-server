b8774cdc9ed141b08d4af1088539249f808ecc72
z-dayao <zalvendayao888@gmail.com>
2025-09-26 13:47:47 +0800

    feat: computation type for loan

--- src/service/service.transaction.go
+++ src/service/service.transaction.go
@@ -65,13 +65,6 @@ func (t *TransactionService) Deposit(ctx context.Context, account TransactionDat
 	}
 }
 
-// g5454324
-// Liability, Equity & Revenue = Credit
-
-// AccountTypeDeposit
-// Asset & Expense = Debit
-
-// Withdraw implements TransactionService.
 func (t *TransactionService) Withdraw(ctx context.Context, account TransactionData, amount float64) (credit, debit, balance float64, err error) {
 	if account.Account == nil {
 		return 0, 0, 0, eris.New("account is required")
@@ -124,4 +117,57 @@ func (t *TransactionService) ComputeTotalBalance(context context.Context, genera
 		debit += gl.Debit
 	}
 	return credit, debit, math.Abs(credit - debit), nil
+}
+
+func (t *TransactionService) LoanComputaton(ctx context.Context, ald model.AutomaticLoanDeduction, lt model.LoanTransaction) float64 {
+	result := lt.Applied1
+	// --- Min/Max check ---
+	if ald.MinAmount > 0 && result < ald.MinAmount {
+		return 0.0
+	}
+
+	if ald.MaxAmount > 0 && result > ald.MaxAmount {
+		return 0.0
+	}
+	// --- Percentage application ---
+	if ald.ChargesPercentage1 > 0 || ald.ChargesPercentage2 > 0 {
+		if ald.ChargesPercentage1 > 0 && ald.ChargesPercentage2 > 0 {
+			if ald.AddOn {
+				result *= ald.ChargesPercentage2 / 100
+			} else {
+				result *= ald.ChargesPercentage1 / 100
+			}
+		} else if ald.ChargesPercentage1 > 0 {
+			result *= ald.ChargesPercentage1 / 100
+		} else {
+			result *= ald.ChargesPercentage2 / 100
+		}
+	}
+
+	// --- Divisor application ---
+	if ald.ChargesDivisor > 0 && result > 0 {
+		result = (result / ald.ChargesDivisor) * ald.ChargesAmount
+	}
+
+	// --- Annum adjustments (when months = 0) ---
+	if ald.NumberOfMonths == 0 {
+		if ald.Anum == 1 || ald.Anum == -1 {
+			result /= 12
+		} else if ald.Anum > 1 {
+			result /= float64(ald.Anum)
+		}
+	}
+
+	// --- Number of months adjustments ---
+	if ald.NumberOfMonths == -1 {
+		result = (result * float64(lt.Terms)) / 12
+	} else if ald.NumberOfMonths > 0 {
+		result = (result * float64(lt.Terms)) / float64(ald.NumberOfMonths)
+	}
+
+	if result == lt.Applied1 {
+		return ald.ChargesAmount
+	}
+
+	return result
 }
