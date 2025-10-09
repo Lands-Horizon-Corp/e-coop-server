ccc41af69c028d2254a09cdca7c5ecb8f252f3c7
z-dayao <zalvendayao888@gmail.com>
2025-10-09 12:17:07 +0800

    feat: loan transaction

--- src/model/loan_transaction.go
+++ src/model/loan_transaction.go
@@ -573,7 +573,7 @@ type (
 	LoanTransactionSuggestedRequest struct {
 		Amount        float64           `json:"amount" validate:"required,gt=0"`
 		Principal     float64           `json:"principal" validate:"required,gt=0"`
-		ModeOfPayment LoanModeOfPayment `json:"mode_of_payment" validate:"required,oneof='SINGLE' 'WEEKLY' 'SEMI-MONTHLY' 'FIXED DAYS' 'MONTHLY'"`
+		ModeOfPayment LoanModeOfPayment `json:"mode_of_payment"`
 		FixedDays     int               `json:"fixed_days,omitempty" validate:"omitempty,gte=1"`
 	}
 	LoanTransactionSuggestedResponse struct {
