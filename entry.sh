c18125775c485665d0eb9e088d0fefd9ac2a3712
z-dayao <zalvendayao888@gmail.com>
2025-10-27 13:46:23 +0800

    feat: adding subscription card plans with currency

--- entry.sh
+++ entry.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # Clean cache first
-# ./app cache clean
+./app cache clean
 
 # Reset database (drops all tables)
-# ./app db reset
+./app db reset
 
 # Migrate database (recreate tables)
-# ./app db migrate
+./app db migrate
 
 # Seed database (insert initial data)
-# ./app db seed
+./app db seed
 
 # Start your Go app (it will load .env from current directory)
 ./app server
-
-# fly deploy; fly machine stop 148e4d55f36278; fly machine restart 148e4d55f36278; fly machine restart 90802d3ea0ed38; fly logs
