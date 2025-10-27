41a424a5640c99a57af121b724eec877d0124d66
z-dayao <zalvendayao888@gmail.com>
2025-10-27 13:35:47 +0800

    feat: adding entry sh no seed

--- entry.sh
+++ entry.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Clean cache first
-./app cache clean
+# ./app cache clean
 
 # Reset database (drops all tables)
-./app db reset
+# ./app db reset
 
 # Migrate database (recreate tables)
-./app db migrate
+# ./app db migrate
 
 # Seed database (insert initial data)
-./app db seed
+# ./app db seed
 
 # Start your Go app (it will load .env from current directory)
 ./app server
