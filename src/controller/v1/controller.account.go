046b1bebd571a6f9e8007e7c2f8215de43920872
z-dayao <zalvendayao888@gmail.com>
2025-10-30 16:56:42 +0800

    feat: controller account updated

--- src/controller/v1/controller.account.go
+++ src/controller/v1/controller.account.go
@@ -829,8 +829,9 @@ func (c *Controller) AccountController() {
 		})
 		// Event notification
 		c.event.Notification(context, ctx, event.NotificationEvent{
-			Description:       fmt.Sprintf("Account: the account has been updated - %s", account.Name),
-			Title: fmt.Sprintf("Updated account: %s", account.Name),
+			Description:      fmt.Sprintf("Account: the account has been updated - %s", account.Name),
+			Title:            fmt.Sprintf("Updated account: %s", account.Name),
+			NotificationType: "success",
 		})
 
 		return ctx.JSON(http.StatusOK, c.model_core.AccountManager.ToModel(account))
