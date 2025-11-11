fe05ed7e020981865c273aa5109eb4df00d913d6
zalven-official <zalvendayao888@gmail.com>
2025-11-11 11:11:01 +0800

    feat: generated report

--- server/model/core/generated_report.go
+++ server/model/core/generated_report.go
@@ -28,17 +28,20 @@ type (
 		BranchID       uuid.UUID      `gorm:"type:uuid;not null;index:idx_branch_org_generated_report"`
 		Branch         *Branch        `gorm:"foreignKey:BranchID;constraint:OnDelete:CASCADE;" json:"branch,omitempty"`
 
-		UserID       *uuid.UUID `gorm:"type:uuid"`
-		User         *User      `gorm:"foreignKey:UserID;constraint:OnDelete:SET NULL;" json:"user,omitempty"`
-		MediaID      *uuid.UUID `gorm:"type:uuid;not null"`
-		Media        *Media     `gorm:"foreignKey:MediaID"`
-		Name         string     `gorm:"type:varchar(255);not null"`
-		Description  string     `gorm:"type:text;not null"`
-		Status       string     `gorm:"type:varchar(50);not null"`
-		Progress     float64    `gorm:"not null"`
-		FilterSearch string     `gorm:"type:text" json:"filter_search,omitempty"`
-		IsFavorite   bool       `gorm:"type:boolean;default:false" json:"is_favorite"`
-		Model        string     `gorm:"type:varchar(255)" json:"model,omitempty"`
+		UserID            *uuid.UUID `gorm:"type:uuid"`
+		User              *User      `gorm:"foreignKey:UserID;constraint:OnDelete:SET NULL;" json:"user,omitempty"`
+		MediaID           *uuid.UUID `gorm:"type:uuid;not null"`
+		Media             *Media     `gorm:"foreignKey:MediaID"`
+		Name              string     `gorm:"type:varchar(255);not null"`
+		Description       string     `gorm:"type:text;not null"`
+		Status            string     `gorm:"type:varchar(50);not null"`
+		Progress          float64    `gorm:"not null"`
+		FilterSearch      string     `gorm:"type:text" json:"filter_search,omitempty"`
+		IsFavorite        bool       `gorm:"type:boolean;default:false" json:"is_favorite"`
+		Model             string     `gorm:"type:varchar(255)" json:"model,omitempty"`
+		DownloadSpeedKB   float64    `gorm:"type:decimal;default:0" json:"download_speed_kb"`
+		ReportSizeBytes   int64      `gorm:"type:bigint;default:0" json:"report_size_bytes"`
+		CurrentDownloaded int64      `gorm:"type:bigint;default:0" json:"current_downloaded"`
 	}
 	// GeneratedReportResponse represents the response structure for generatedreport data
 	GeneratedReportResponse struct {
@@ -54,26 +57,32 @@ type (
 		BranchID       uuid.UUID             `json:"branch_id"`
 		Branch         *BranchResponse       `json:"branch,omitempty"`
 
-		UserID      *uuid.UUID     `json:"user_id"`
-		User        *UserResponse  `json:"user"`
-		MediaID     *uuid.UUID     `json:"media_id"`
-		Media       *MediaResponse `json:"media,omitempty"`
-		Name        string         `json:"name"`
-		Description string         `json:"description"`
-		Status      string         `json:"status"`
-		Progress    float64        `json:"progress"`
-		IsFavorite  bool           `json:"is_favorite"`
-		Model       string         `json:"model,omitempty"`
+		UserID            *uuid.UUID     `json:"user_id"`
+		User              *UserResponse  `json:"user"`
+		MediaID           *uuid.UUID     `json:"media_id"`
+		Media             *MediaResponse `json:"media,omitempty"`
+		Name              string         `json:"name"`
+		Description       string         `json:"description"`
+		Status            string         `json:"status"`
+		Progress          float64        `json:"progress"`
+		IsFavorite        bool           `json:"is_favorite"`
+		Model             string         `json:"model,omitempty"`
+		DownloadSpeedKB   float64        `json:"download_speed_kb"`
+		ReportSizeBytes   int64          `json:"report_size_bytes"`
+		CurrentDownloaded int64          `json:"current_downloaded"`
 	}
 
 	// GeneratedReportRequest represents the request structure for GeneratedReport.
 	GeneratedReportRequest struct {
-		ID           *uuid.UUID `json:"id,omitempty"`
-		Name         string     `json:"firstName" validate:"required,min=1,max=255"`
-		Description  string     `json:"description" validate:"required,min=1"`
-		FilterSearch string     `json:"filter_search,omitempty"`
-		IsFavorite   bool       `json:"is_favorite,omitempty"`
-		Model        string     `json:"model,omitempty"`
+		ID                *uuid.UUID `json:"id,omitempty"`
+		Name              string     `json:"name" validate:"required,min=1,max=255"`
+		Description       string     `json:"description" validate:"required,min=1"`
+		FilterSearch      string     `json:"filter_search,omitempty"`
+		IsFavorite        bool       `json:"is_favorite,omitempty"`
+		Model             string     `json:"model,omitempty"`
+		DownloadSpeedKB   float64    `json:"download_speed_kb,omitempty"`
+		ReportSizeBytes   int64      `json:"report_size_bytes,omitempty"`
+		CurrentDownloaded int64      `json:"current_downloaded,omitempty"`
 	}
 )
 
@@ -99,16 +108,19 @@ func (m *Core) generatedReport() {
 				BranchID:       data.BranchID,
 				Branch:         m.BranchManager.ToModel(data.Branch),
 
-				UserID:      data.UserID,
-				User:        m.UserManager.ToModel(data.User),
-				MediaID:     data.MediaID,
-				Media:       m.MediaManager.ToModel(data.Media),
-				Name:        data.Name,
-				Description: data.Description,
-				Status:      data.Status,
-				Progress:    data.Progress,
-				IsFavorite:  data.IsFavorite,
-				Model:       data.Model,
+				UserID:            data.UserID,
+				User:              m.UserManager.ToModel(data.User),
+				MediaID:           data.MediaID,
+				Media:             m.MediaManager.ToModel(data.Media),
+				Name:              data.Name,
+				Description:       data.Description,
+				Status:            data.Status,
+				Progress:          data.Progress,
+				IsFavorite:        data.IsFavorite,
+				Model:             data.Model,
+				DownloadSpeedKB:   data.DownloadSpeedKB,
+				ReportSizeBytes:   data.ReportSizeBytes,
+				CurrentDownloaded: data.CurrentDownloaded,
 			}
 		},
 		Created: func(data *GeneratedReport) []string {
