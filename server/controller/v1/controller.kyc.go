cc070ef20fa7f930700e0f05870aa8f232162d44
zalven-official <zalvendayao888@gmail.com>
2025-12-24 10:00:01 +0800

    feat: KYC Request

--- server/controller/v1/controller.kyc.go
+++ server/controller/v1/controller.kyc.go
@@ -2,18 +2,12 @@ package v1
 
 func (c *Controller) kyxController() {
 	// POST /api/v1/kyc/personal-details
-
 	// POST /api/v1/kyc/security-details
-
 	// POST /api/v1/kyc/verify-email
-
 	// POST /api/v1/kyc/verify-contact-number
-
 	// POST /api/v1/kyc/verify-addresses
-
 	// POST /api/v1/kyc/verify-government-benefits
-
 	// POST /api/v1/kyc/face-recognize
-
+	// POST /api/v1/kyc/selfie
 	// POST /api/v1/kyc/register
 }
