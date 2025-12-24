f3836d2095fcdcf4cf025045c404eb55190a40ba
zalven-official <zalvendayao888@gmail.com>
2025-12-24 09:43:51 +0800

    fix: KYC Controller register

---
+++ server/model/core/kyc.go
@@ -0,0 +1,12 @@
+package core
+
+type (
+	KYCPersonalDetails          struct{}
+	KYCSecurityDetails          struct{}
+	KYCVerifyEmail              struct{}
+	KYCVerifyContactNumber      struct{}
+	KYCVerifyAddresses          struct{}
+	KYCVerifyGovernmentBenefits struct{}
+	KYCFaceRecognize            struct{}
+	KYCRegister                 struct{}
+)
