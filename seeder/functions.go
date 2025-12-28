8a665b63e89c1d27e5df12d84872020187d86f73
zalven-official <zalvendayao888@gmail.com>
2025-12-28 11:08:56 +0800

    chore: uncomment seeder

--- seeder/functions.go
+++ seeder/functions.go
@@ -1,90 +1,90 @@
 package seeder
 
-// import (
-// 	"context"
-// 	crand "crypto/rand"
-// 	"io/fs"
-// 	"math/big"
-// 	"path/filepath"
-// 	"strings"
-// 	"time"
+import (
+	"context"
+	crand "crypto/rand"
+	"io/fs"
+	"math/big"
+	"path/filepath"
+	"strings"
+	"time"
 
-// 	"github.com/Lands-Horizon-Corp/e-coop-server/server/model/core"
-// 	"github.com/Lands-Horizon-Corp/e-coop-server/services/horizon"
-// 	"github.com/rotisserie/eris"
-// )
+	"github.com/Lands-Horizon-Corp/e-coop-server/server/model/core"
+	"github.com/Lands-Horizon-Corp/e-coop-server/services/horizon"
+	"github.com/rotisserie/eris"
+)
 
-// func (s *Seeder) loadImagePaths() error {
-// 	imagesDir := "seeder/images"
-// 	supportedExtensions := map[string]bool{
-// 		".jpg":  true,
-// 		".jpeg": true,
-// 		".png":  true,
-// 		".webp": true,
-// 	}
+func (s *Seeder) loadImagePaths() error {
+	imagesDir := "seeder/images"
+	supportedExtensions := map[string]bool{
+		".jpg":  true,
+		".jpeg": true,
+		".png":  true,
+		".webp": true,
+	}
 
-// 	err := filepath.WalkDir(imagesDir, func(path string, d fs.DirEntry, err error) error {
-// 		if err != nil {
-// 			return err
-// 		}
+	err := filepath.WalkDir(imagesDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
 
-// 		if !d.IsDir() {
-// 			ext := strings.ToLower(filepath.Ext(path))
-// 			if supportedExtensions[ext] {
-// 				s.imagePaths = append(s.imagePaths, path)
-// 			}
-// 		}
+		if !d.IsDir() {
+			ext := strings.ToLower(filepath.Ext(path))
+			if supportedExtensions[ext] {
+				s.imagePaths = append(s.imagePaths, path)
+			}
+		}
 
-// 		return nil
-// 	})
+		return nil
+	})
 
-// 	if err != nil {
-// 		return eris.Wrap(err, "failed to scan images directory")
-// 	}
+	if err != nil {
+		return eris.Wrap(err, "failed to scan images directory")
+	}
 
-// 	if len(s.imagePaths) == 0 {
-// 		return eris.New("no image files found in seeder/images directory")
-// 	}
+	if len(s.imagePaths) == 0 {
+		return eris.New("no image files found in seeder/images directory")
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// func (s *Seeder) createImageMedia(ctx context.Context, imageType string) (*core.Media, error) {
-// 	if len(s.imagePaths) == 0 {
-// 		return nil, eris.New("no image files available for seeding")
-// 	}
+func (s *Seeder) createImageMedia(ctx context.Context, imageType string) (*core.Media, error) {
+	if len(s.imagePaths) == 0 {
+		return nil, eris.New("no image files available for seeding")
+	}
 
-// 	maxInt := big.NewInt(int64(len(s.imagePaths)))
-// 	nBig, err := crand.Int(crand.Reader, maxInt)
-// 	if err != nil {
-// 		return nil, eris.Wrap(err, "failed to generate secure random index for image selection")
-// 	}
-// 	randomIndex := int(nBig.Int64())
-// 	imagePath := s.imagePaths[randomIndex]
+	maxInt := big.NewInt(int64(len(s.imagePaths)))
+	nBig, err := crand.Int(crand.Reader, maxInt)
+	if err != nil {
+		return nil, eris.Wrap(err, "failed to generate secure random index for image selection")
+	}
+	randomIndex := int(nBig.Int64())
+	imagePath := s.imagePaths[randomIndex]
 
-// 	storage, err := s.provider.Service.Storage.UploadFromPath(ctx, imagePath, func(_ int64, _ int64, _ *horizon.Storage) {})
-// 	if err != nil {
-// 		return nil, eris.Wrapf(err, "failed to upload image from path %s for %s", imagePath, imageType)
-// 	} // Create media record
-// 	media := &core.Media{
-// 		FileName:   storage.FileName,
-// 		FileType:   storage.FileType,
-// 		FileSize:   storage.FileSize,
-// 		StorageKey: storage.StorageKey,
-// 		BucketName: storage.BucketName,
-// 		Status:     "completed",
-// 		Progress:   100,
-// 		CreatedAt:  time.Now().UTC(),
-// 		UpdatedAt:  time.Now().UTC(),
-// 	}
+	storage, err := s.provider.Service.Storage.UploadFromPath(ctx, imagePath, func(_ int64, _ int64, _ *horizon.Storage) {})
+	if err != nil {
+		return nil, eris.Wrapf(err, "failed to upload image from path %s for %s", imagePath, imageType)
+	} // Create media record
+	media := &core.Media{
+		FileName:   storage.FileName,
+		FileType:   storage.FileType,
+		FileSize:   storage.FileSize,
+		StorageKey: storage.StorageKey,
+		BucketName: storage.BucketName,
+		Status:     "completed",
+		Progress:   100,
+		CreatedAt:  time.Now().UTC(),
+		UpdatedAt:  time.Now().UTC(),
+	}
 
-// 	if err := s.core.MediaManager().Create(ctx, media); err != nil {
-// 		return nil, eris.Wrap(err, "failed to create media record")
-// 	}
+	if err := s.core.MediaManager().Create(ctx, media); err != nil {
+		return nil, eris.Wrap(err, "failed to create media record")
+	}
 
-// 	return media, nil
-// }
+	return media, nil
+}
 
-// func ptr[T any](v T) *T {
-// 	return &v
-// }
+func ptr[T any](v T) *T {
+	return &v
+}
