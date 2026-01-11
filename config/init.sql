63e62ee09d4b839e9b77d94a0f9b7c5d1f991f5d
zalven-official <zalvendayao888@gmail.com>
2026-01-11 21:59:51 +0800

    initial commit

--- config/init.sql
+++
@@ -1,5 +0,0 @@
--- Enable the pgcrypto extension to use gen_random_uuid
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
--- Enable the vector extension for vector operations
-CREATE EXTENSION IF NOT EXISTS vector;
