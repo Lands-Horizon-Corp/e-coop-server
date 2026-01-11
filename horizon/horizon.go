0cba39f858743e965944ec755e21a243d208a798
zalven-official <zalvendayao888@gmail.com>
2026-01-12 00:47:02 +0800

    feat: adding horizon service

--- horizon/horizon.go
+++ horizon/horizon.go
@@ -1,1 +1,28 @@
 package horizon
+
+import (
+	"github.com/go-playground/validator/v10"
+	"go.uber.org/zap"
+)
+
+type HorizonService struct {
+	API      *APIImpl
+	Broker   *MessageBrokerImpl
+	Cache    *CacheImpl
+	Config   *ConfigImpl
+	Database *DatabaseImpl
+	OTP      *OTPImpl
+	QR       *QRImpl
+	Schedule *ScheduleImpl
+	Security *SecurityImpl
+	SMS      *SMSImpl
+	SMTP     *SMTPImpl
+	Storage  *StorageImpl
+
+	Validator *validator.Validate
+	Logger    *zap.Logger
+}
+
+func NewHorizonService() *HorizonService {
+	return &HorizonService{}
+}
