2b155435f9cf4264cdc37f37bd3ef386e1902579
zalven-official <zalvendayao888@gmail.com>
2026-01-12 02:47:48 +0800

    feat: adding horizon logs and fix env and pkg

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"context"
 
+	"github.com/Lands-Horizon-Corp/e-coop-server/core"
 	"github.com/Lands-Horizon-Corp/e-coop-server/horizon"
 )
 
 func main() {
 	service := horizon.NewHorizonService()
 	if err := service.Run(context.Background()); err != nil {
+		panic(err)
+	}
+
+	core := core.NewCore(service)
+	if err := core.Start(); err != nil {
 		panic(err)
 	}
 }
