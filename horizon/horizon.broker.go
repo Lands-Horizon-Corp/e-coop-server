882214414394840cbe71e81ff9b75ed0a9370e58
zalven-official <zalvendayao888@gmail.com>
2026-01-11 22:28:43 +0800

    feat: adding horizon broker

--- horizon/horizon.broker.go
+++ horizon/horizon.broker.go
@@ -1,1 +1,110 @@
 package horizon
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+	"github.com/rotisserie/eris"
+)
+
+type MessageBrokerImpl struct {
+	host     string
+	port     int
+	nc       *nats.Conn
+	clientID string
+	natsUser string
+	natsPass string
+}
+
+func NewHorizonMessageBroker(host string, port int, clientID, natsUser, natsPass string) MessageBrokerImpl {
+	return MessageBrokerImpl{
+		host:     host,
+		port:     port,
+		clientID: clientID,
+		natsUser: natsUser,
+		natsPass: natsPass,
+	}
+}
+
+func (h *MessageBrokerImpl) Run() error {
+	natsURL := fmt.Sprintf("nats://%s:%d", h.host, h.port)
+	options := []nats.Option{
+		nats.UserInfo(h.natsUser, h.natsPass),
+		nats.ErrorHandler(func(_ *nats.Conn, sub *nats.Subscription, err error) {
+			fmt.Printf("Error in subscription to %s: %v\n", sub.Subject, err)
+		}),
+	}
+
+	nc, err := nats.Connect(natsURL, options...)
+	if err != nil {
+		return eris.Wrap(err, "failed to connect to NATS")
+	}
+	h.nc = nc
+	return nil
+}
+
+func (h *MessageBrokerImpl) Stop() error {
+	if h.nc != nil {
+		h.nc.Close()
+		h.nc = nil
+	}
+	return nil
+}
+
+func (h *MessageBrokerImpl) Dispatch(topics []string, payload any) error {
+	if h.nc == nil {
+		return eris.New("NATS connection not initialized")
+	}
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return eris.Wrap(err, "failed to marshal payload")
+	}
+	msg := &nats.Msg{Data: data}
+	for _, topic := range topics {
+		msg.Subject = h.formatTopic(topic)
+		if err := h.nc.PublishMsg(msg); err != nil {
+			return eris.Wrap(err, fmt.Sprintf("failed to publish to topic %s", topic))
+		}
+	}
+	return nil
+}
+
+func (h *MessageBrokerImpl) Publish(topic string, payload any) error {
+	if h.nc == nil {
+		return eris.New("NATS connection not initialized")
+	}
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return eris.Wrap(err, "failed to marshal payload")
+	}
+	if err := h.nc.PublishMsg(&nats.Msg{Subject: h.formatTopic(topic), Data: data}); err != nil {
+		return eris.Wrap(err, fmt.Sprintf("failed to publish to topic %s", topic))
+	}
+	return nil
+}
+
+func (h *MessageBrokerImpl) Subscribe(topic string, handler func(any) error) error {
+	if h.nc == nil {
+		return eris.New("NATS connection not initialized")
+	}
+	_, err := h.nc.Subscribe(h.formatTopic(topic), func(msg *nats.Msg) {
+		var payload map[string]any
+		if err := json.Unmarshal(msg.Data, &payload); err != nil {
+			fmt.Printf("Failed to unmarshal message from topic %s: %v\n", topic, err)
+			return
+		}
+		if err := handler(payload); err != nil {
+			fmt.Printf("Handler error for topic %s: %v\n", topic, err)
+		}
+	})
+	if err != nil {
+		return eris.Wrap(err, fmt.Sprintf("failed to subscribe to topic %s", topic))
+	}
+	return nil
+}
+
+func (h *MessageBrokerImpl) formatTopic(topic string) string {
+	return fmt.Sprintf("%s.%s", h.clientID, topic)
+}
