2106b0c2e07764df2d3521f3b661f1473db116d6
zalven-official <zalvendayao888@gmail.com>
2026-01-21 19:41:04 +0800

    fix: branch settings

--- src/core/user_organization.go
+++ src/core/user_organization.go
@@ -39,8 +39,6 @@ func UserOrganizationManager(service *horizon.HorizonService) *registry.Registry
 			"Branch.BranchSetting.CashOnHandAccount.Currency",
 			"Branch.BranchSetting.PaidUpSharedCapitalAccount",
 			"Branch.BranchSetting.PaidUpSharedCapitalAccount.Currency",
-			"Branch.BranchSetting.DefaultMemberGender",
-			"Branch.BranchSetting.DefaultMemberType",
 
 			"Branch.BranchSetting.CompassionFundAccount",
 			"Branch.BranchSetting.CompassionFundAccount.Currency",
@@ -48,7 +46,6 @@ func UserOrganizationManager(service *horizon.HorizonService) *registry.Registry
 			"Branch.BranchSetting.UnbalancedAccounts.Currency",
 			"Branch.BranchSetting.UnbalancedAccounts.AccountForShortage",
 			"Branch.BranchSetting.UnbalancedAccounts.AccountForOverage",
-			"Branch.BranchSetting.UnbalancedAccounts.CashOnHandAccount",
 			"Branch.BranchSetting.UnbalancedAccounts.MemberProfileForShortage",
 			"Branch.BranchSetting.UnbalancedAccounts.MemberProfileForOverage",
 		},
