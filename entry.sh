d879d9b6e72d92c579f935c91a26538a2394e863
zalven-official <zalvendayao888@gmail.com>
2026-01-27 14:54:08 +0800

    feat: enhanced actions descriptions

--- entry.sh
+++ entry.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-# Clean cache first
-./app cache clean
+./app cache-clean
 
-# Enforce security blocklist
-./app security enforce
+./app security-enforce-blocklist
 
-# Reset database (drops all tables)
-./app db reset
+./app db-reset
 
-# Migrate database (recreate tables)
-./app db migrate
+./app db-migrate
 
-# Seed database (insert initial data)
-./app db seed
+./app db-seed
 
-# Start your Go app (it will load .env from current directory)
 ./app server
